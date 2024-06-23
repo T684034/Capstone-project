d99fdc8f232b5fcf030fa3c98b4d68f20517059b
Alexandre Bevilacqua <>
2024-06-23 11:30:38 +0200

    First commit

---
+++ Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Alexandre Bevilacqua on 23.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
